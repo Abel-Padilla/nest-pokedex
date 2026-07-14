6ee944d0a35ac70f744417472ab8df2b8a6d8bd8
Abel Padilla <abel.soria98@gmail.com>
2026-07-14 17:53:45 -0600

    Added dynamic connection to the mongoDB cluster

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,7 +21,9 @@ import { JoiValidationSchema } from './config/joi.validation';
       rootPath: join(__dirname, '..', 'public'),
     }),
 
-    MongooseModule.forRoot(process.env.MONGO_DB!),
+    MongooseModule.forRoot(process.env.MONGO_DB! ,{
+      dbName : 'pkemonsdb'
+    }),
 
     PokemonModule,
 
